76043aa90c71d056814ba6abcd6c318c49de3ef9
Vol4enak <52600927+Vol4enak@users.noreply.github.com>
2023-04-09 12:27:57 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import { ContactSlice } from './contactLogic';
@@ -16,6 +25,12 @@ const persistedContactReducer = persistReducer(
 
 export const store = configureStore({
   reducer: { phoneBook: persistedContactReducer },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
